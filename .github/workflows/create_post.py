1e59591249cbeb281a4f503264525d5fd4e2dc97
Marcel <marehr@users.noreply.github.com>
2023-09-04 18:10:06 +0200

    Update create_post.py

--- .github/workflows/create_post.py
+++ .github/workflows/create_post.py
@@ -1,16 +1,35 @@
+from string import Template
+
+post_template = Template("""
+---
+layout: elternbrief
+title: "${title}"
+date: "${date}"
+category: elternbrief
+---
+
+${post_body}
+
+""")
+
 def main():
     import os
-    issue_id = os.environ['ISSUE_ID']
-    issue_title = os.environ['ISSUE_TITLE']
-    issue_body = os.environ['ISSUE_BODY']
-    issue_created_at = os.environ['ISSUE_CREATED_AT']
-    issue_updated_at = os.environ['ISSUE_UPDATED_AT']
 
-    print (f"issue_id: {issue_id}")
-    print (f"issue_title: {issue_title}")
-    print (f"issue_body: {issue_body}")
-    print (f"issue_created_at: {issue_created_at}")
-    print (f"issue_updated_at: {issue_updated_at}")
+    post = {
+        id: os.environ['ISSUE_ID']
+        title: os.environ['ISSUE_TITLE']
+        body: os.environ['ISSUE_BODY']
+        created_at: os.environ['ISSUE_CREATED_AT']
+        updated_at: os.environ['ISSUE_UPDATED_AT']
+    }
+
+    print (f"post_id: {post.id}")
+    print (f"post_title: {post.title}")
+    print (f"post_body: {post.body}")
+    print (f"post_created_at: {post.created_at}")
+    print (f"post_updated_at: {post.updated_at}")
+
+    print (post_template.substitute(**post))
 
 if __name__ == "__main__":
     main()
