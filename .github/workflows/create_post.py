2cf42242bbaf8ee5d1efb66cdb667c9c9f2e052a
Marcel <marehr@users.noreply.github.com>
2023-09-04 21:21:02 +0200

    Update create_post.py

--- .github/workflows/create_post.py
+++ .github/workflows/create_post.py
@@ -28,14 +28,23 @@ def main():
         'updated_at': parse_date(os.environ['ISSUE_UPDATED_AT']), # 2023-09-04T15:34:54Z
     }
 
+    def post_filename(post):
+        from slugify import slugify
+        # YEAR-MONTH-DAY-title.MARKUP
+        date_str = post['created_at'].strftime("%Y-%m-%d")
+        slug_title = slugify(post['title'])
+        return f'{date_str}-{slug_title}'
+
     template_post = dict(**post)
     template_post['date'] = template_post['created_at'].strftime("%Y-%m-%d %H:%M:%S") # YYYY-MM-DD HH:MM:SS
+    template_post['filename'] = post_filename(template_post)
 
     print (f"post_id: {post['id']}")
     print (f"post_title: {post['title']}")
     print (f"post_body: {post['body']}")
     print (f"post_created_at: {post['created_at']}")
     print (f"post_updated_at: {post['updated_at']}")
+    print (f"filename: {template_post['filename']}")
 
     print (post_template.substitute(**template_post))
 
