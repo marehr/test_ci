56ded45537ace5e85f1507736566a4eebee61b77
Marcel <marehr@users.noreply.github.com>
2023-09-04 18:01:15 +0200

    Update create_post.py

--- .github/workflows/create_post.py
+++ .github/workflows/create_post.py
@@ -1,8 +1,16 @@
 def main():
     import os
     issue_id = os.environ['ISSUE_ID']
-    print (f"issue_id: {issue_id}")
+    issue_title = os.environ['ISSUE_TITLE']
+    issue_body = os.environ['ISSUE_BODY']
+    issue_created_at = os.environ['ISSUE_CREATED_AT']
+    issue_updated_at = os.environ['ISSUE_UPDATED_AT']
 
+    print (f"issue_id: {issue_id}")
+    print (f"issue_title: {issue_title}")
+    print (f"issue_body: {issue_body}")
+    print (f"issue_created_at: {issue_created_at}")
+    print (f"issue_updated_at: {issue_updated_at}")
 
 if __name__ == "__main__":
     main()
