dc9f6b6c3f0cda6bbf4718c42219fe5e5b26e728
Marcel <marehr@users.noreply.github.com>
2023-09-04 20:55:27 +0200

    Update create_post.py

--- .github/workflows/create_post.py
+++ .github/workflows/create_post.py
@@ -15,13 +15,21 @@ ${post_body}
 def main():
     import os
 
+    def parse_date(date_str):
+        import dateutil.parser
+        datetime = dateutil.parser.isoparse(date_str)
+        return datetime
+
     post = {
         'id': os.environ['ISSUE_ID'], 
         'title': os.environ['ISSUE_TITLE'], 
         'body': os.environ['ISSUE_BODY'], 
-        'created_at': os.environ['ISSUE_CREATED_AT'], 
-        'updated_at': os.environ['ISSUE_UPDATED_AT'], 
+        'created_at': parse_date(os.environ['ISSUE_CREATED_AT']), # 2023-09-04T15:34:54Z
+        'updated_at': parse_date(os.environ['ISSUE_UPDATED_AT']), # 2023-09-04T15:34:54Z
     }
+
+    template_post = dict(**post)
+    template_post['date'] = template_post['created_at'].strftime("YYYY-MM-DD HH:MM:SS") # YYYY-MM-DD HH:MM:SS
 
     print (f"post_id: {post['id']}")
     print (f"post_title: {post['title']}")
@@ -29,7 +37,7 @@ def main():
     print (f"post_created_at: {post['created_at']}")
     print (f"post_updated_at: {post['updated_at']}")
 
-    print (post_template.substitute(**post))
+    print (post_template.substitute(**template_post))
 
 if __name__ == "__main__":
     main()
