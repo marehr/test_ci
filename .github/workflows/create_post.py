677e54bb00110d9298dae643b35170bf810e4b91
Marcel <marehr@users.noreply.github.com>
2023-09-04 18:14:48 +0200

    Update create_post.py

--- .github/workflows/create_post.py
+++ .github/workflows/create_post.py
@@ -23,7 +23,7 @@ def main():
         'updated_at': os.environ['ISSUE_UPDATED_AT'], 
     }
 
-    print (f"post_id: {post['id']']}")
+    print (f"post_id: {post['id']}")
     print (f"post_title: {post['title']}")
     print (f"post_body: {post['body']}")
     print (f"post_created_at: {post['created_at']}")
